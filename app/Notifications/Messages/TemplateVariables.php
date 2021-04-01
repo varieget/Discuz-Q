b3ebf847b37733216b6c56dfedd9fdbbdd8696f4
Harvey <619596123@qq.com>
2021-04-01 11:10:48 +0800

    fix: 修改通知升级时出现唯一索引 bug 问题
    (cherry picked from commit af3ed346603392791fc95357d42690633e7d04c5)

--- app/Notifications/Messages/TemplateVariables.php
+++ app/Notifications/Messages/TemplateVariables.php
@@ -242,9 +242,9 @@ trait TemplateVariables
      *
      * @param string $typeName
      * @param int $type
-     * @return string
+     * @return string|null
      */
-    protected function comparisonUnique(string $typeName, int $type) : string
+    protected function comparisonUnique(string $typeName, int $type)
     {
         // 通知类型: 0系统 1微信 2短信 3企业微信 4小程序通知
         switch ($type) {
@@ -263,6 +263,14 @@ trait TemplateVariables
                 break;
         }
 
-        return $prefix . array_search($typeName, $this->uniquelyNotice);
+        // 搜索定义的唯一标识名单
+        $result = array_search($typeName, $this->uniquelyNotice);
+
+        // 未搜索到时返回 null 值
+        if (! $result) {
+            return null;
+        }
+
+        return $prefix . $result;
     }
 }
