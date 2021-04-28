8b12f0b80cc9ef11498557f6ce826e1bb1ff1d64
cjwjkcqw <13627305664@163.com>
2021-04-28 10:59:35 +0800

    cjw:回调地址替换测试

--- app/Commands/Trade/PayOrder.php
+++ app/Commands/Trade/PayOrder.php
@@ -269,7 +269,8 @@ class PayOrder
             case Order::PAYMENT_TYPE_WECHAT_JS: //微信网页、公众号
             case Order::PAYMENT_TYPE_WECHAT_MINI: //微信小程序支付
                 $config = $this->setting->tag('wxpay'); //配置信息
-                $config['notify_url'] = $this->url->to('/api/trade/notify/wechat');
+                // $config['notify_url'] = $this->url->to('/api/trade/notify/wechat');
+                $config['notify_url'] = $this->url->to('/apiv3/trade/notify/wechat');
                 switch ($this->payment_type) {
                     case Order::PAYMENT_TYPE_WECHAT_NATIVE: //微信扫码支付
                         $pay_gateway          = GatewayConfig::WECAHT_PAY_NATIVE;
