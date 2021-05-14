b27c1310b8be466c03ed44962d19f8bf622fdce8
husbind <295935172@qq.com>
2021-05-14 17:45:55 +0800

    关注列表参数修改

--- app/Api/Controller/UsersV3/ListUserFollowController.php
+++ app/Api/Controller/UsersV3/ListUserFollowController.php
@@ -58,13 +58,17 @@ class ListUserFollowController extends DzqController
             $groups = array_column($groups, null, 'user_id');
             $users = User::instance()->getUsers($userIds);
 
-            $userFollow = UserFollow::query()->where('from_user_id', $this->user->id)->get()->toArray();
+            $userFollow = UserFollow::query()
+                ->where('from_user_id', $actor->id)
+                ->get()->toArray();
             $userFollow = array_column($userFollow, null, 'to_user_id');
 
             foreach ($userFollowList as $key => $value) {
                 $userFollowList[$key]['isFollow']       = false;
+                $userFollowList[$key]['isMutual'] = false;
                 if (isset($userFollow[$value['to_user_id']])) {
-                    $userFollowList[$key]['isFollow']  = true;
+                    $userFollowList[$key]['isFollow']       = true;
+                    $userFollowList[$key]['isMutual'] = (bool) $userFollow[$value['to_user_id']]['is_mutual'];
                 }
             }
 
@@ -94,7 +98,7 @@ class ListUserFollowController extends DzqController
                 $lists['id'] = $list['id'];
                 $lists['fromUserId'] = $list['from_user_id'];
                 $lists['toUserId'] = $list['to_user_id'];
-                $lists['isMutual'] = $list['is_mutual'];
+                $lists['isMutual'] = $list['isMutual'];
                 $lists['createdAt'] = $list['created_at'];
                 $lists['updatedAt'] = $list['updated_at'];
                 $lists['isFollow'] = $list['isFollow'];
@@ -120,9 +124,9 @@ class ListUserFollowController extends DzqController
         $user = '';
         $query = $this->userFollow->query()->select('user_follow.*')->distinct();
 
-        $type = (int) Arr::get($filter, 'type', 0);
+        $type = (int) Arr::get($filter, 'type', 1);
         $username = Arr::get($filter, 'userName');
-        if ($user_id = (int)Arr::get($filter, 'user_id')) {
+        if ($user_id = (int)Arr::get($filter, 'userId')) {
             $user = $this->user->findOrFail($user_id);
         }
         $user_id = $user ? $user->id : $actor->id;
