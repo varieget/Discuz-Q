e8b0208f194c1b8eb85c3222655c1ad4d2feefb9
richardtu <richardtu@tencent.com>
2021-06-09 17:38:00 +0800

    Revert "!1098 完善 红包帖/悬赏帖 逻辑；逻辑流程：order.create --> thread.create --> pay.order --> or"
    This reverts commit 8ceb3e13e538b737bbe5298cb63eb2f682607bf9, reversing
    changes made to ce920e6240bf5471b234ad913a77cb0801d46d27.

--- app/Modules/ThreadTom/Busi/RewardBusi.php
+++ app/Modules/ThreadTom/Busi/RewardBusi.php
@@ -27,8 +27,6 @@ use App\Models\ThreadTom;
 
 class RewardBusi extends TomBaseBusi
 {
-    public const NEED_PAY = 1;
-
     public function create()
     {
         $input = $this->verification();
@@ -37,17 +35,14 @@ class RewardBusi extends TomBaseBusi
         }
 
         $threadReward = ThreadReward::query()->where('thread_id', $this->threadId)->first();
-        /*
+
         if (!empty($threadReward)) {
             $thread = Thread::query()->where('id', $this->threadId)->first(['is_draft']);
             if ($thread->is_draft == Thread::IS_NOT_DRAFT) $this->outPut(ResponseCode::INVALID_PARAMETER,'已发布的悬赏不可编辑');
         }
-        */
 
-        if ($input['draft'] == Thread::IS_DRAFT) {
-            if(empty($input['orderSn'])){
-                $this->outPut(ResponseCode::INVALID_PARAMETER, '红包缺少orderSn');
-            }
+        if ($input['draft'] != Thread::IS_DRAFT) {
+
             $order = Order::query()
                 ->where('order_sn',$input['orderSn'])
                 ->first(['id','thread_id','user_id','status','amount','expired_at','type']);
@@ -55,7 +50,7 @@ class RewardBusi extends TomBaseBusi
             if (empty($order) ||
                 ($order->type == Order::ORDER_TYPE_QUESTION_REWARD && !empty($order['thread_id'])) ||
                 $order['user_id'] != $this->user['id'] ||
-                $order['status'] != Order::ORDER_STATUS_PENDING ||
+                $order['status'] != Order::ORDER_STATUS_PAID ||
                 (!empty($order['expired_at']) && strtotime($order['expired_at']) < time())||
                 ($order->type == Order::ORDER_TYPE_QUESTION_REWARD && $order->amount != $input['price'])) {
                 $this->outPut(ResponseCode::INVALID_PARAMETER);
@@ -68,7 +63,7 @@ class RewardBusi extends TomBaseBusi
                     ->first();
                 if (empty($orderChildrenInfo) ||
                     $orderChildrenInfo->amount != $input['price'] ||
-                    $orderChildrenInfo->status != Order::ORDER_STATUS_PENDING) {
+                    $orderChildrenInfo->status != Order::ORDER_STATUS_PAID) {
                     $this->outPut(ResponseCode::INVALID_PARAMETER);
                 }
             }
