1ceaaed9373a41faecb5968c5d1b4808494c2237
Discuz <f15507470940>
2021-06-11 22:17:57 +0800

    文件下载

--- app/Api/Controller/AttachmentV3/DownloadAttachmentController.php
+++ app/Api/Controller/AttachmentV3/DownloadAttachmentController.php
@@ -85,21 +85,11 @@ class DownloadAttachmentController extends DzqController
             'updated_at' => Carbon::now()
         ]);
 
-        //声明浏览器输出的是字节流
-        header('Content-Type: application/octet-stream');
-        //声明浏览器返回大小是按字节进行计算
-        header('Accept-Ranges:bytes');
-        //告诉浏览器文件的总大小
-        $fileSize = filesize($url);//坑 filesize 如果超过2G 低版本php会返回负数
-        header('Content-Length:' . $fileSize); //注意是'Content-Length:' 非Accept-Length
-        //声明下载文件的名称
-        header('Content-Disposition:attachment;filename=' . basename($attachment->file_name));//声明作为附件处理和下载后文件的名称
-        //获取文件内容
-        $handle = fopen($url, 'rb');//二进制文件用‘rb’模式读取
-        while (!feof($handle) ) { //循环到文件末尾 规定每次读取（向浏览器输出为$readBuffer设置的字节数）
-            echo fread($handle, 10240);
-        }
-        fclose($handle);//关闭文件句柄
-        exit;
+        header("Content-type:application/octet-stream");
+        $origin_name = basename($attachment->file_name);
+        header("Content-Disposition:attachment;filename = " . $origin_name);
+        header("Accept-ranges:bytes");
+        header("Accept-length:" . $attachment->file_size);
+        readfile($url);
     }
 }
