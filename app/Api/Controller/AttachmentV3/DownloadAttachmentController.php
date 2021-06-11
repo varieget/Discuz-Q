fbf585e980b87591b495d9f237925260b355f6cc
Discuz <f15507470940>
2021-06-11 17:48:13 +0800

    下载文件修改

--- app/Api/Controller/AttachmentV3/DownloadAttachmentController.php
+++ app/Api/Controller/AttachmentV3/DownloadAttachmentController.php
@@ -83,11 +83,36 @@ class DownloadAttachmentController extends DzqController
             'updated_at' => Carbon::now()
         ]);
 
-        //文件的类型
-        header('Content-type: application/octstream');
-        //下载显示的名字
-        header('Content-Disposition: attachment; filename='.$attachment->file_name);
-        readfile("$url");
-        exit();
+        //文件完整路径（这里将真实的文件存放在temp目录下）
+        $filePath = $url;
+        //以只读方式打开文件，并强制使用二进制模式
+        $fileHandle = fopen($filePath,"rb");
+        if ( !empty($fileHandle) ) {
+            ob_clean();//清除一下缓冲区
+            //获得文件名称
+            $filename = basename(urldecode($attachment->file_name));
+            //将utf8编码转换成gbk编码，否则，文件中文名称的文件无法打开
+            $filePath = iconv('UTF-8','gbk',$filePath);
+            /**
+             * 这里应该加上安全验证之类的代码，例如：检测请求来源、验证UA标识等等
+             */
+            //文件类型是二进制流。设置为utf8编码（支持中文文件名称）
+            header('Content-type:application/octet-stream; charset=utf-8');
+            header("Content-Transfer-Encoding: binary");
+            header("Accept-Ranges: bytes");
+            //文件大小
+            header("Content-Length: ".filesize($filePath));
+            //触发浏览器文件下载功能
+            header('Content-Disposition:attachment;filename="'.urlencode($filename).'"');
+            //循环读取文件内容，并输出
+            while(!feof($fileHandle)) {
+                //从文件指针 handle 读取最多 length 个字节（每次输出10k）
+                echo fread($fileHandle, 10240);
+            }
+            //关闭文件流
+            fclose($fileHandle);
+        }
+
+        $this->outPut(ResponseCode::RESOURCE_NOT_FOUND);
     }
 }
