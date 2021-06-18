35af37f5d990463fb3cb818cfc2fe080082fea2a
MrVinceLee <765249023@qq.com>
2021-06-18 13:24:55 +0800

    小程序跳转页面修改

--- app/Api/Controller/UsersV3/MiniProgramQrcodeController.php
+++ app/Api/Controller/UsersV3/MiniProgramQrcodeController.php
@@ -44,8 +44,8 @@ class MiniProgramQrcodeController extends AuthBaseController
      */
     //todo 对接前端时更换路由
     static $qrcodeTypeAndRouteMap = [
-        'pc_login_mini'              => '/pages/user/pc-login',
-        'pc_bind_mini'               => '/pages/user/pc-relation',
+        'pc_login_mini'              => '/subPages/user/wx-authorization/index',
+        'pc_bind_mini'               => '/subPages/user/wx-auth/index',
     ];
     /**
      * 二维码生成类型与token标识映射
