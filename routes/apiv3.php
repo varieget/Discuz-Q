e729e31167b42e0fe59eda9cb570e3d472ba19f1
MrVinceLee <765249023@qq.com>
2021-06-24 23:45:00 +0800

    添加接口层日志接口

--- routes/apiv3.php
+++ routes/apiv3.php
@@ -193,3 +193,4 @@ $route->get('/offiaccount/jssdk', 'offiaccount.jssdk', ApiController\WechatV3\Of
 
 $route->get('/thread.test', 'thread.test', ApiController\ThreadsV3\TestController::class);
 
+$route->post('/open.api.log', 'open.api.log', ApiController\SettingsV3\OpenApiLogController::class);
