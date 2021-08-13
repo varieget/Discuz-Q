1006c6b14944dbe7a99f9f670460e258296054ee
dengjingyuan <602754659@qq.com>
2021-08-13 18:19:41 +0800

    增加qcloud_daily

---
+++ app/Listeners/User/QcloudDaily.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Copyright (C) 2020 Tencent Cloud.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace App\Listeners\User;
+
+use App\Settings\SettingsRepository;
+use App\Events\Setting\Saved;
+use Illuminate\Support\Arr;
+use Discuz\Qcloud\QcloudTrait;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class QcloudDaily
+{
+    use QcloudTrait;
+
+    /**
+     * @var Request
+     */
+    public $request;
+
+    /**
+     * @var SettingsRepository
+     */
+    public $settings;
+
+    /**
+     * @param Request $request
+     * @param SettingsRepository $settings
+     */
+    public function __construct(Request $request, SettingsRepository $settings)
+    {
+        $this->request = $request;
+        $this->settings = $settings;
+    }
+
+    /**
+     * @param Saved $event
+     */
+    public function handle(Saved $event)
+    {
+
+    }
+
+}
