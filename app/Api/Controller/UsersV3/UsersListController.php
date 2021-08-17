63de5721b6b7ab10b5f10932846337b6b682f11b
流火行者 <coralchu@tencent.com>
2021-08-17 08:50:39 +0800

    Revert "!1699 【0819】添加userId返回"
    This reverts commit ae020248bd0468951812dbccd83f4ab9d90820d0, reversing
    changes made to d4652e3a8a592390bce1285f75da449d06db9362.

--- app/Api/Controller/UsersV3/UsersListController.php
+++ app/Api/Controller/UsersV3/UsersListController.php
@@ -70,7 +70,6 @@ class UsersListController extends DzqController
             $userId = $userData['id'];
             $groupId = $groupUsers[$userId]['group_id'];
             $item = [
-                'userId'=>$userId,
                 'nickname' => $userData['nickname'],
                 'username' => $userData['username'],
                 'avatar' => $userData['avatar'],
