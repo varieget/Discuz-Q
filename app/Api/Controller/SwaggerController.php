740d3cc7ecc3894db894c4af9474bbfaab26e52e
流火行者 <coralchu@tencent.com>
2021-08-31 19:14:31 +0800

    添加swagger控制器

--- app/Api/Controller/SwaggerController.php
+++ app/Api/Controller/SwaggerController.php
@@ -1,13 +1,12 @@
 <?php
-
-/*
- * Copyright (C) 2020 Tencent Cloud.
+/**
+ * Copyright (C) 2021 Tencent Cloud.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,12 +15,19 @@
  * limitations under the License.
  */
 
-define('DISCUZ_START', microtime(true));
+namespace App\Api\Controller;
 
-require __DIR__ . '/../vendor/autoload.php';
 
-$app = new Discuz\Foundation\Application(dirname(__DIR__));
+use Discuz\Base\DzqController;
 
-$app->singleton(Discuz\Http\Server::class, Discuz\Http\Server::class);
+class SwaggerController extends DzqController
+{
 
-$app->make(Discuz\Http\Server::class)->listen();
+    public function main()
+    {
+        $path = __DIR__ . '/../../ApiDoc';
+        $openApi = \OpenApi\Generator::scan([$path]);
+        header('Content-Type: application/json');
+        exit($openApi->toJson());
+    }
+}
