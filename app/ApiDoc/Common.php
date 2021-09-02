9c2c67bab7f39ad123601897062db803956d94f9
流火行者 <coralchu@tencent.com>
2021-09-02 11:11:04 +0800

    no message

--- app/ApiDoc/Common.php
+++ app/ApiDoc/Common.php
@@ -298,7 +298,7 @@
  *     ),
  *     @OA\Property(property = "content", type = "object", description = "帖子正文内容",
  *        @OA\Property(property = "text", type = "string", description = "帖子正文内容"),
- *        @OA\Property(property = "indexes", type = "object", description = "是否可删除帖子",
+ *        @OA\Property(property = "indexes", type = "object", description = "插件集合",
  *        @OA\Property(property = "101", type = "object", description = "图片",ref="#/components/schemas/local_plugin_output"),
  *        @OA\Property(property = "102", type = "object", description = "语音",ref="#/components/schemas/local_plugin_output"),
  *        @OA\Property(property = "103", type = "string", description = "视频",ref="#/components/schemas/local_plugin_output"),
@@ -308,5 +308,33 @@
  *        @OA\Property(property = "108", type = "string", description = "文件附件",ref="#/components/schemas/local_plugin_output"),
  *        @OA\Property(property = "109", type = "string", description = "投票",ref="#/components/schemas/local_plugin_output"),
  *      )))
+ * )
+ * @OA\Schema(
+ *     schema="thread_indexes_input",
+ *     title="主题内含基础插件信息",
+ *     @OA\Property(property="indexes",type="object",description="插件数据集合",
+ *        @OA\Property(property = "101", type = "object", description = "图片",ref="#/components/schemas/local_plugin_input"),
+ *        @OA\Property(property = "102", type = "object", description = "语音",ref="#/components/schemas/local_plugin_input"),
+ *        @OA\Property(property = "103", type = "string", description = "视频",ref="#/components/schemas/local_plugin_input"),
+ *        @OA\Property(property = "104", type = "string", description = "商品",ref="#/components/schemas/local_plugin_input"),
+ *        @OA\Property(property = "106", type = "string", description = "红包",ref="#/components/schemas/local_plugin_input"),
+ *        @OA\Property(property = "107", type = "string", description = "悬赏",ref="#/components/schemas/local_plugin_input"),
+ *        @OA\Property(property = "108", type = "string", description = "文件附件",ref="#/components/schemas/local_plugin_input"),
+ *        @OA\Property(property = "109", type = "string", description = "投票",ref="#/components/schemas/local_plugin_input"),
+ *     )
+ * )
+ * @OA\Schema(
+ *     schema="thread_indexes_output",
+ *     title="主题内含基础插件信息",
+ *     @OA\Property(property="indexes",type="object",description="插件数据集合",
+ *        @OA\Property(property = "101", type = "object", description = "图片",ref="#/components/schemas/local_plugin_output"),
+ *        @OA\Property(property = "102", type = "object", description = "语音",ref="#/components/schemas/local_plugin_output"),
+ *        @OA\Property(property = "103", type = "string", description = "视频",ref="#/components/schemas/local_plugin_output"),
+ *        @OA\Property(property = "104", type = "string", description = "商品",ref="#/components/schemas/local_plugin_output"),
+ *        @OA\Property(property = "106", type = "string", description = "红包",ref="#/components/schemas/local_plugin_output"),
+ *        @OA\Property(property = "107", type = "string", description = "悬赏",ref="#/components/schemas/local_plugin_output"),
+ *        @OA\Property(property = "108", type = "string", description = "文件附件",ref="#/components/schemas/local_plugin_output"),
+ *        @OA\Property(property = "109", type = "string", description = "投票",ref="#/components/schemas/local_plugin_output"),
+ *     )
  * )
  */
