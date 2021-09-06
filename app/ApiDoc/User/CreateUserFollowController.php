792366a9cee480ba8ee5d31759672acd0eb22213
dengjingyuan <602754659@qq.com>
2021-09-06 16:11:49 +0800

    增加  follow  swagger

---
+++ app/ApiDoc/User/CreateUserFollowController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *@OA\Post(
+ *    path = "/apiv3/follow",
+ *    summary = "个人中心",
+ *    description = "Discuz! Q 关注/粉丝列表",
+ *    tags ={"个人中心"},
+ *    @OA\Parameter(ref = "#/components/parameters/bear_token"),
+ *     @OA\RequestBody(
+ *        required=true,
+ *        description = "关注用户id",
+ *        @OA\JsonContent(
+ *           @OA\Property(property="toUserId",type="integer",description="关注用户id")
+ *        )
+ *     ),
+ *
+ * @OA\Response(
+ *        response = 200,
+ *        description = "",
+ *        @OA\JsonContent(allOf ={
+ *                @OA\Schema(ref = "#/components/schemas/dzq_layout")
+ *            }
+ *        )
+ *    )
+ *)
+ *
+ *
+ *
+ */
+
