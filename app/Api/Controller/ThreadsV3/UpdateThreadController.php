b88df97481b8976ab9e855fde5dee6ec8afc4eea
ahuchjm <ahuchjm@yeah.net>
2021-09-07 21:42:21 +0800

    后端添加复核权限逻辑

--- app/Api/Controller/ThreadsV3/UpdateThreadController.php
+++ app/Api/Controller/ThreadsV3/UpdateThreadController.php
@@ -50,6 +50,7 @@ class UpdateThreadController extends DzqController
 
     protected function checkRequestPermissions(UserRepository $userRepo)
     {
+        $this->checkThreadPluginAuth($userRepo);
         $this->thread = $this->thread = Thread::query()
             ->where(['id' => $this->inPut('threadId')])
             ->whereNull('deleted_at')
