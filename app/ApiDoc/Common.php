d564d52b0e6e19d4e11ec410601992233bd32829
dengjingyuan <602754659@qq.com>
2021-09-09 12:17:31 +0800

    继续完善

--- app/ApiDoc/Common.php
+++ app/ApiDoc/Common.php
@@ -401,22 +401,7 @@
  *      )
  * ),
  *
- * @OA\Schema(
- *     schema="dzq_group_simple_detail",
- *     title="用户组详情",
- *     @OA\Property(property = "groupIcon", type="string", description = "用户组icon"),
- *     @OA\Property(property = "groupName", type="string", description = "用户组名称"),
- *     @OA\Property(property = "pid", type="integer", description = "用户组id"),
- * )
  *
- * @OA\Schema(
- *     schema="dzq_user_simple_detail",
- *     title="用户详情",
- *     @OA\Property(property = "avatar", type="string", description = "用户头像url"),
- *     @OA\Property(property = "nickName", type="string", description = "用户昵称"),
- *     @OA\Property(property = "pid", type="integer", description = "用户id"),
- *     @OA\Property(property = "userName", type="string", description = "用户名称"),
- * )
  * @OA\Schema(
  *     schema = "post_detail_output",
  *     title = "评论详情输出数据集合",
@@ -653,18 +638,6 @@
  * )
  *
  *
- *  @OA\Schema(
- *     schema="dzq_user_follow_simple_detail",
- *     title="关注/粉丝用户详情",
- *     description="关注/粉丝用户详情",
- *     @OA\Property(property = "createdAt", type="string", description = "创建时间", format = "datetime"),
- *     @OA\Property(property = "fromUserId", type="integer", description = "关注人id"),
- *     @OA\Property(property = "id", type="integer", description = "关注id"),
- *     @OA\Property(property = "isFollow", type="integer", description = "是否关注过别人/被别人关注过"),
- *     @OA\Property(property = "isMutual", type="integer", description = "是否互关"),
- *     @OA\Property(property = "toUserId", type="integer", description = "被关注人id"),
- *     @OA\Property(property = "updatedAt", type="string", format = "datetime", description = "更新时间"),
- * )
  *
  *
  *
