75428ae2f54540a4010d9af6108b0390f3df5a98
流火行者 <coralchu@tencent.com>
2021-09-10 13:00:54 +0800

    no message

---
+++ plugin/activity/database/migrations/2021_09_10_112512_plugin_activity_thread_activity.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Plugin\Activity\database\migrations;
+
+use Discuz\Base\DzqPluginMigration;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+
+class PluginActivityThreadActivity extends DzqPluginMigration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema()->create('plugin_activity_thread_activity', function (Blueprint $table) {
+            $table->unsignedBigInteger('id', true)->comment('自增id');
+            $table->string('title', 100)->nullable(false)->comment('活动名称');
+            $table->text('content')->comment('活动内容');
+            $table->timestamp('activity_start_time')->comment('活动开始时间');
+            $table->timestamp('activity_end_time')->comment('活动结束时间');
+            $table->timestamp('register_start_time')->comment('报名开始时间');
+            $table->timestamp('register_end_time')->comment('报名结束时间');
+            $table->string('address', 200)->comment('地址信息');
+            $table->string('location', 200)->comment('位置信息');
+            $table->decimal('longitude', 10, 7)->default(0.0000000)->nullable(false)->comment('经度');
+            $table->decimal('latitude', 10, 7)->default(0.0000000)->nullable(false)->comment('纬度');
+            $table->tinyInteger('status')->default(1)->comment('0:无效 1：有效');
+            $table->timestamp('created_at')->nullable(false)->default(new Expression('CURRENT_TIMESTAMP'))->comment('创建时间');
+            $table->timestamp('updated_at')->nullable(false)->default(new Expression('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comment('更新时间');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema()->dropIfExists('plugin_activity_thread_activity');
+    }
+}
