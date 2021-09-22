b62f0033dee5095ed831a450b07647a4af95c0b3
wentguo <1553690085@qq.com>
2021-09-22 13:02:11 +0800

    用户组，添加 特权描述字段

---
+++ database/migrations/2021_09_22_125749_add_describe_to_groups.php
@@ -0,0 +1,31 @@
+<?php
+
+use Discuz\Database\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddDescribeToGroups extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema()->table('groups', function (Blueprint $table) {
+            $table->string('description', 255)->default('')->after('is_commission')->comment('特权描述');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema()->table('groups', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+}
