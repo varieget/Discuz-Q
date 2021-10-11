57ca820c70cf568fa2c3b4c869c75faaf8319f61
流火行者 <coralchu@tencent.com>
2021-10-11 11:38:52 +0800

    插件类型设置

--- app/Common/PluginEnum.php
+++ app/Common/PluginEnum.php
@@ -18,16 +18,10 @@
 namespace App\Common;
 
 
-class DzqConst
+class PluginEnum
 {
-    //数据库bool类型查询
-    const BOOL_YES = 1;
-    const BOOL_NO = 0;
-
-    //帖子列表接口作用域
-    const SCOPE_NORMAL = 0;//常规列表
-    const SCOPE_RECOMMEND = 1;//推荐列表
-    const SCOPE_SEARCH = 2;//发现页搜索列表
-    const SCOPE_PAID = 3;//付费站首页列表
-
+    const PLUGIN_THREAD = 1;//主体扩展对象插件
+    const PLUGIN_DATA_IMPORT = 2;//外部数据导入
+    const PLUGIN_AD = 3;//广告插件
+    const PLUGIN_BANNER = 4;//首页banner插件
 }
