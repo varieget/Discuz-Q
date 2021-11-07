c59f7a142380db8ef84a5ec0508e0ed6d8123d4d
ahuchjm <ahuchjm@yeah.net>
2021-11-07 12:12:10 +0800

    no message

--- plugin/Activity/Routes/Api.php
+++ plugin/Activity/Routes/Api.php
@@ -11,11 +11,6 @@ $route->withFrequency(function (\Discuz\Http\RouteCollection $route) {
     $route->get('register/list', 'register.list', \Plugin\Activity\Controller\ListController::class);
 //报名用户信息导出
     $route->get('register/export', 'register.export', \Plugin\Activity\Controller\ExportController::class);
-//测试接口替换
-    $route->get('register/thread.list', 'register.thread.list', \Plugin\Activity\Controller\ThreadListV1Controller::class, \App\Api\Controller\Threads\ThreadListController::class);
-
-    $route->get('register/thread.create', 'register.thread.create', \Plugin\Activity\Controller\ThreadListV1Controller::class, \App\Api\Controller\Threads\ThreadListController::class);
-
 }, 30, 60);
 
 
