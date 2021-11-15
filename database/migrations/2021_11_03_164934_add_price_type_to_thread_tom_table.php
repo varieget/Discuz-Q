b0843b3bc35c301bffef7b2903d87f0036b6e219
dengjingyuan <602754659@qq.com>
2021-11-15 14:45:26 +0800

    1

--- database/migrations/2021_11_03_164934_add_price_type_to_thread_tom_table.php
+++ database/migrations/2021_11_03_164934_add_price_type_to_thread_tom_table.php
@@ -13,8 +13,8 @@ class AddPriceTypeToThreadTomTable extends Migration
     public function up()
     {
         $this->schema()->table('thread_tom', function (Blueprint $table) {
-            $table->tinyInteger('price_type')->default('0')->comment('插件/组件是否部分付费');
-            $table->string('price_ids')->default('{}')->comment('插件/组件部分付费id集合');
+            $table->tinyInteger('price_type')->default('0')->after('status')->comment('插件/组件是否部分付费');
+            $table->string('price_ids')->default('{}')->after('price_type')->comment('插件/组件部分付费id集合');
         });
     }
 
