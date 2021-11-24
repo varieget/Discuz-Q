f47713b0305c258403fb7490d5806a15975c7853
Apple <apple@AppledeMacBook-Pro.local>
2021-11-24 16:10:15 +0800

    @1125-createAttachment优化

--- app/Api/Controller/Attachment/CreateAttachmentController.php
+++ app/Api/Controller/Attachment/CreateAttachmentController.php
@@ -56,6 +56,14 @@ class CreateAttachmentController extends DzqController
 
     protected $settings;
 
+    private $file;
+    private $fileName;
+    private $ext;
+    private $tmpFile;
+    private $tmpFileWithExt;
+    private $fileType;
+    private $fileSize;
+
     public function __construct(Dispatcher $events, AttachmentValidator $validator, AttachmentUploader $uploader, SettingsRepository $settings)
     {
         $this->events = $events;
@@ -111,101 +119,57 @@ class CreateAttachmentController extends DzqController
         $order = (int) Arr::get($this->request->getParsedBody(), 'order', 0);
         $ipAddress = ip($this->request->getServerParams());
         $mediaId = Arr::get($this->request->getParsedBody(), 'mediaId', '');
-
         $request = $this->request->getParsedBody();
         if (isset($request['fileUrl']) && empty($request['fileUrl'])) {
             $this->outPut(ResponseCode::INVALID_PARAMETER, '图片链接不可为空');
         }
-
         $fileUrl = Arr::get($this->request->getParsedBody(), 'fileUrl', '');
 
 //        ini_set('memory_limit',-1);
 
         if (!empty($mediaId)) {
-            $app = $this->offiaccount();
-            $mediaFile = $app->media->get($mediaId);
-            if ($mediaFile instanceof \EasyWeChat\Kernel\Http\StreamResponse) {
-                $file = $mediaFile->save(storage_path('/tmp'));
-                $fileName = basename($file);
-                $ext = pathinfo($file, PATHINFO_EXTENSION);
-                $tmpFileWithExt = storage_path('/tmp') .'/' . $fileName;
-                $imageSize = getimagesize($tmpFileWithExt);
-                $fileType = $imageSize['mime'];
-                $fileSize = filesize($tmpFileWithExt);
-            }
-        } elseif (!empty($fileUrl)) {
-            $urlContent = Utils::downLoadFile($fileUrl);
-            if (!$urlContent) {
-                $this->outPut(ResponseCode::INVALID_PARAMETER, '未获取到文件内容');
-            }
-            //URL链接图处理
-            $fileType = $this->getAttachmentMimeType($fileUrl);
-            $allowedMimeType = $this->getAllowedMimeType($type);
-            if (!in_array($fileType, $allowedMimeType)) {
-                $this->outPut(ResponseCode::INVALID_PARAMETER, $fileType . ' 类型文件不允许上传');
-            }
-
-            $ext = $this->getFileExt($type, $fileType);
-            $fileName = urldecode(basename($fileUrl));
-            if (strpos($fileName, '?') !== false) {
-                $fileName = substr($fileName, 0, strpos($fileName, '?'));
-                if (strpos($fileName, '/') !== false) {
-                    $fileName = substr($fileName, strrpos($fileName, '/') + 1, strlen($fileName));
-                }
-            }
-            if (strpos($fileName, '.') === false) {
-                $fileName  = $fileName . '.' . $ext;
-            }
-            $tmp_file_path = storage_path('tmp').'/'.$fileName.'_'.uniqid().'.'.$ext;
-            while (file_exists($tmp_file_path)) {
-                $tmp_file_path = storage_path('tmp').'/'.$fileName.'_'.uniqid().'.'.$ext;
-            }
-            $tmp_file = fopen($tmp_file_path, 'w');
-            $file = $tmpFileWithExt = $real_file_path = realpath($tmp_file_path);
-            fwrite($tmp_file, $urlContent);
-            fclose($tmp_file);
-            $fileSize = filesize($real_file_path);
-        } else {
-            $fileName = $file->getClientFilename();
-            $fileSize = $file->getSize();
-            $fileType = $file->getClientMediaType();
-            $ext = pathinfo($fileName, PATHINFO_EXTENSION);
-            $tmpFile = tempnam(storage_path('/tmp'), 'attachment');
-            $tmpFileWithExt = $tmpFile . ($ext ? ".$ext" : '');
+            $this->getMediaFile($mediaId);
+        }
+        if (!empty($fileUrl)) {
+            $this->getFileFromUrl($fileUrl, $type);
+        }
+        if (empty($mediaId) && empty($fileUrl)) {
+            $this->getConventionalFile($file);
         }
 
         //上传临时目录之前验证
         $this->validator->valid([
             'type' => $type,
-            'file' => $file,
-            'size' => $fileSize,
-            'ext' => strtolower($ext),
+            'file' => $this->file,
+            'size' => $this->fileSize,
+            'ext' => strtolower($this->ext),
         ]);
+
         // 从微信下载的文件不需要再移动
         if (!$mediaId && !$fileUrl) {
-            $file->moveTo($tmpFileWithExt);
+            $this->file->moveTo($this->tmpFileWithExt);
         }
 
         try {
             if (!$mediaId && !$fileUrl) {
                 $file = new UploadedFile(
-                    $tmpFileWithExt,
-                    $fileName,
-                    $fileType,
-                    $file->getError(),
+                    $this->tmpFileWithExt,
+                    $this->fileName,
+                    $this->fileType,
+                    $this->file->getError(),
                     true
                 );
             } else {
                 $file = new UploadedFile(
-                    $tmpFileWithExt,
-                    $fileName,
-                    $fileType,
+                    $this->tmpFileWithExt,
+                    $this->fileName,
+                    $this->fileType,
                     null,
                     true
                 );
             }
 
-            if (strtolower($ext) != 'gif' && $fileType != 'image/gif') {
+            if (strtolower($this->ext) != 'gif' && $this->fileType != 'image/gif') {
                 $this->events->dispatch(
                     new Uploading($actor, $file)
                 );
@@ -220,7 +184,7 @@ class CreateAttachmentController extends DzqController
             $width = 0;
             $height = 0;
             if (in_array($type, [Attachment::TYPE_OF_IMAGE, Attachment::TYPE_OF_DIALOG_MESSAGE, Attachment::TYPE_OF_ANSWER])) {
-                list($width, $height) = getimagesize($tmpFileWithExt);
+                list($width, $height) = getimagesize($this->tmpFileWithExt);
             }
 
             $attachment = Attachment::build(
@@ -252,40 +216,107 @@ class CreateAttachmentController extends DzqController
             ], $e->getMessage());
             $this->outPut(ResponseCode::INTERNAL_ERROR, '附件上传异常', [$e->getMessage()]);
         } finally {
-            @unlink($tmpFile);
-            @unlink($tmpFileWithExt);
+            @unlink($this->tmpFile);
+            @unlink($this->tmpFileWithExt);
         }
         $attachmentSerializer = $this->app->make(AttachmentSerializer::class);
         $attachment = $attachmentSerializer->getDefaultAttributes($attachment);
         $data = $this->camelData($attachment);
 
         if (!empty($dialogMessageId)) {
-            $message_text = [
-                'message_text'  => null,
-                'image_url'     => $data['url']
-            ];
-            $message_text = addslashes(json_encode($message_text));
-            $updateDialogMessageResult = DialogMessage::query()
-                ->where('id', $dialogMessageId)
-                ->update(['attachment_id' => $data['id'], 'message_text' => $message_text, 'status' => DialogMessage::NORMAL_MESSAGE]);
-            if (!$updateDialogMessageResult) {
-                $this->outPut(ResponseCode::INTERNAL_ERROR, '私信图片更新失败');
-            } else {
-                $dialogMessage = DialogMessage::query()->where('id', $dialogMessageId)->first();
-                $dialog = Dialog::query()->where('id', $dialogMessage->dialog_id)->first();
-                $lastDialogMessage = DialogMessage::query()->where('id', $dialog->dialog_message_id)->first();
-                if ($dialog->dialog_message_id == 0 ||
-                   (isset($lastDialogMessage['created_at']) && ($lastDialogMessage['created_at'] < $dialogMessage['created_at']))) {
-                    $updateDialogResult = Dialog::query()
-                        ->where('id', $dialogMessage->dialog_id)
-                        ->update(['dialog_message_id' => $dialogMessage->id]);
-                    if (!$updateDialogResult) {
-                        $this->outPut(ResponseCode::INTERNAL_ERROR, '最新对话更新失败');
-                    }
-                }
-            }
+            $this->updateDialogMessage($dialogMessageId, $data);
         }
 
         $this->outPut(ResponseCode::SUCCESS, '', $data);
+    }
+
+    private function getMediaFile($mediaId)
+    {
+        $app = $this->offiaccount();
+        $mediaFile = $app->media->get($mediaId);
+        if ($mediaFile instanceof \EasyWeChat\Kernel\Http\StreamResponse) {
+            $this->file = $mediaFile->save(storage_path('/tmp'));
+            $this->fileName = basename($this->file);
+            $this->ext = pathinfo($this->file, PATHINFO_EXTENSION);
+            $this->tmpFileWithExt = storage_path('/tmp') .'/' . $this->fileName;
+            $imageSize = getimagesize($this->tmpFileWithExt);
+            $this->fileType = $imageSize['mime'];
+            $this->fileSize = filesize($this->tmpFileWithExt);
+        }
+    }
+
+    private function getFileFromUrl($fileUrl, $type)
+    {
+        $urlContent = Utils::downLoadFile($fileUrl);
+        if (!$urlContent) {
+            $this->outPut(ResponseCode::INVALID_PARAMETER, '未获取到文件内容');
+        }
+
+        // url链接图处理
+        $this->fileType = $this->getAttachmentMimeType($fileUrl);
+        $allowedMimeType = $this->getAllowedMimeType($type);
+        if (!in_array($this->fileType, $allowedMimeType)) {
+            $this->outPut(ResponseCode::INVALID_PARAMETER, $this->fileType . ' 类型文件不允许上传');
+        }
+
+        $this->ext = $this->getFileExt($type, $this->fileType);
+
+        // 文件名兼容处理
+        $this->fileName = urldecode(basename($fileUrl));
+        if (strpos($this->fileName, '?') !== false) {
+            $this->fileName = substr($this->fileName, 0, strpos($this->fileName, '?'));
+            if (strpos($this->fileName, '/') !== false) {
+                $this->fileName = substr($this->fileName, strrpos($this->fileName, '/') + 1, strlen($this->fileName));
+            }
+        }
+        if (strpos($this->fileName, '.') === false) {
+            $this->fileName  = $this->fileName . '.' . $this->ext;
+        }
+
+        $this->tmpFile = $tmpFile = tempnam(storage_path('/tmp'), 'attachment');
+        $this->tmpFileWithExt = $this->tmpFile . '.' . $this->ext;
+        @file_put_contents($this->tmpFileWithExt, $urlContent);
+        $this->fileSize = filesize($this->tmpFileWithExt);
+        $this->file = $urlContent;
+    }
+
+    private function getConventionalFile($file)
+    {
+        $this->file = $file;
+        $this->fileName = $this->file->getClientFilename();
+        $this->fileSize = $this->file->getSize();
+        $this->fileType = $this->file->getClientMediaType();
+        $this->ext = pathinfo($this->fileName, PATHINFO_EXTENSION);
+        $this->tmpFile = tempnam(storage_path('/tmp'), 'attachment');
+        $this->tmpFileWithExt = $this->tmpFile . ($this->ext ? ".$this->ext" : '');
+    }
+
+    private function updateDialogMessage($dialogMessageId, $data)
+    {
+        $message_text = [
+            'message_text'  => null,
+            'image_url'     => $data['url']
+        ];
+        $message_text = addslashes(json_encode($message_text));
+        $updateDialogMessageResult = DialogMessage::query()
+            ->where('id', $dialogMessageId)
+            ->update(['attachment_id' => $data['id'], 'message_text' => $message_text, 'status' => DialogMessage::NORMAL_MESSAGE]);
+        if (!$updateDialogMessageResult) {
+            $this->outPut(ResponseCode::INTERNAL_ERROR, '私信图片更新失败');
+        } else {
+            $dialogMessage = DialogMessage::query()->where('id', $dialogMessageId)->first();
+            $dialog = Dialog::query()->where('id', $dialogMessage->dialog_id)->first();
+            $lastDialogMessage = DialogMessage::query()->where('id', $dialog->dialog_message_id)->first();
+            if ($dialog->dialog_message_id == 0 ||
+                (isset($lastDialogMessage['created_at']) && ($lastDialogMessage['created_at'] < $dialogMessage['created_at']))) {
+                $updateDialogResult = Dialog::query()
+                    ->where('id', $dialogMessage->dialog_id)
+                    ->update(['dialog_message_id' => $dialogMessage->id]);
+                if (!$updateDialogResult) {
+                    $this->outPut(ResponseCode::INTERNAL_ERROR, '最新对话更新失败');
+                }
+            }
+        }
+        return true;
     }
 }
